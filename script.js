cb6cf277ca9bf001b7b023f346911f3a00b33b44
Sailyt23 <saikoushikyt@gmail.com>
2025-03-31 15:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.to("#rolling-text", { 
+        x: "-100%", 
+        duration: 10, 
+        ease: "linear", 
+        repeat: -1 
+    });
 
+    gsap.from("#title", { opacity: 0, y: -50, duration: 1 });
+    gsap.from("#subtitle", { opacity: 0, y: 20, delay: 0.5, duration: 1 });
+    gsap.from(".project", { opacity: 0, stagger: 0.3, y: 50, duration: 1 });
+});
